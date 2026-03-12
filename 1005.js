67816a806fa6a818f01c28776b979874ca158437
unknown <vitugasbriel@gmail.com>
2026-03-12 10:46:05 -0300

    1005 corrected

--- 1005.js
+++ 1005.js
@@ -4,6 +4,6 @@ var lines = input.split('\n');
 var A = parseFloat(lines.shift());
 var B = parseFloat(lines.shift());
 
-var MEDIA = (A * 3.5) + (B * 7.5) / 11;
+var MEDIA = (A * 3.5 + B * 7.5) / 11;
 
-console.log(`MEDIA = ${MEDIA}`);
+console.log(`MEDIA = ${MEDIA.toFixed(5)}`);
